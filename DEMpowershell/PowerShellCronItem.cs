991a7a5a335bdc362e104d4d36294f4ad97afed9
neolithos <neolithos@outlook.com>
2015-11-30 01:17:03 +0100

    Initial commit

---
+++ DEMpowershell/PowerShellCronItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TecWare.DE.Server;
+
+namespace TecWare.DE
+{
+	///////////////////////////////////////////////////////////////////////////////
+	/// <summary></summary>
+	public class PowerShellCronItem : CronJobItem
+	{
+		public PowerShellCronItem(IServiceProvider sp, string name)
+			: base(sp, name)
+		{
+		} // ctor
+
+		protected override void OnRunJob(CancellationToken cancellation)
+		{
+		} // proc OnRunJob
+	} // class PowerShellCronItem
+}
