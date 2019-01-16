4282419ee347b8e2b150ef8991483b13248122ef
neolithos <neolithos@outlook.com>
2019-01-16 12:57:10 +0100

    Fix: AssemblyInfo

---
+++ DEMpowershell/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+using TecWare.DE;
+using TecWare.DE.Server.Configuration;
+
+[assembly: ComVisible(false)]
+
+[assembly: DEConfigurationSchema(typeof(PowerShellCronItem), "DEMpowershell.xsd")]
+[assembly: Guid("0496732b-0812-4e06-b7fe-212f2761ebaf")]
